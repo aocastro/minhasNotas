c421228d223b40a8a5aa48d0200eab6567c21314
aocastro <adriano.castro2@etec.sp.gov.br>
2020-08-12 19:34:31 -0300

    Commit developer view-disciplinas

--- src/disciplinas/controle/list-disciplinas.js
+++ src/disciplinas/controle/list-disciplinas.js
@@ -10,14 +10,14 @@ $(document).ready(function() {
 
                 let resultado = `
                     <tr>
-                        <td class="text-center d-none d-sm-block">${dados[i].id}</td>
+                        <td class="text-center">${dados[i].id}</td>
                         <td>${dados[i].nome}</td>
-                        <td class="d-none d-sm-block">${dados[i].professor}</td>
+                        <td>${dados[i].professor}</td>
                         <td class="text-center">${dados[i].nota}</td>
                         <td class="text-center">
-                            <button class="btn btn-info btn-sm btn-view"><i class="mdi mdi-eye"></i></button>
-                            <button class="btn btn-primary btn-sm btn-edit"><i class="mdi mdi-pencil"></i></button>
-                            <button class="btn btn-danger btn-sm btn-delete"><i class="mdi mdi-trash-can"></i></button>
+                            <button id="${dados[i].id}" class="btn btn-info btn-sm btn-view"><i class="mdi mdi-eye"></i></button>
+                            <button id="${dados[i].id}" class="btn btn-primary btn-sm btn-edit"><i class="mdi mdi-pencil"></i></button>
+                            <button id="${dados[i].id}" class="btn btn-danger btn-sm btn-delete"><i class="mdi mdi-trash-can"></i></button>
                         </td>
                     </tr>
                 `
@@ -25,6 +25,8 @@ $(document).ready(function() {
                 $('tbody').append(resultado)
 
             }
+
+            $('body').append('<script src="src/disciplinas/controle/view-disciplinas.js"></script>')
         }
     })
 })
