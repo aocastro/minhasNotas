43e4e5acc0a3684a739dd46191e08accc8aa54bb
aocastro <adriano.castro2@etec.sp.gov.br>
2020-09-16 11:26:36 -0300

    Commit de criação de alunos

--- js/function-menu.js
+++ js/function-menu.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
 
+    $('#adiciona-aluno').click(function(e) {
+        e.preventDefault()
+        $('#conteudo').empty()
+        $('#conteudo').load('src/alunos/visao/adiciona-alunos.html')
+    })
+
+    $('#listar-alunos').click(function(e) {
+        e.preventDefault()
+        $('#conteudo').empty()
+        $('#conteudo').load('src/alunos/visao/list-alunos.html')
+    })
+
     $('#adiciona').click(function(e) {
         e.preventDefault()
         $('#conteudo').empty()
