1147d81dd693d1670061aba3b4acfb8e05aafd75
aocastro <adriano.castro2@etec.sp.gov.br>
2020-09-30 10:41:13 -0300

    Commit das funcionalidades dos alunos

---
+++ src/alunos/modelo/delete-alunos.php
@@ -0,0 +1,31 @@
+<?php
+
+    // Iremos conectar nossa função ao banco de dados
+    include('../../conexao/conn.php');
+
+    $id = $_REQUEST['id'];
+
+    // Verificando se os campos foram preenchidos
+    if(strlen($id) == 0){
+        $dados = array(
+            'tipo' => 'alert-warning',
+            'mensagem' => 'Id informado inválido!'
+        );
+    }else{
+        // Criaremos uma variável para receber os comandos SQL
+        $sql = "DELETE FROM alunos WHERE id = ".$id."";
+        // Iremos testar a nossa linha SQL, diretamente no banco de dados
+        if(mysqli_query($conecta, $sql)){
+            $dados = array(
+                'tipo' => 'alert-success',
+                'mensagem' => 'Registro excluído com sucesso!'
+            );
+        }else{
+            $dados = array(
+                'tipo' => 'alert-danger',
+                'mensagem' => 'Erro ao efetuar exclusão do registro.'
+            );
+        }
+    }
+
+    echo json_encode($dados);
